545e11bcaa25780cf6ad3f278357abcf82fab3ac
Geoff Schmit <gcschmit@gmail.com>
2021-09-09 14:13:15 -0500

    start notes on list and iterators

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,5 +9,28 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        /*
+            The addLast method can be used to populate a list.
+        */
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Diana");
+        staff.addLast("Harry");
+        staff.addLast("Romeo");
+        staff.addLast("Tom");
+
+        // The list is currently: DHRT
+
+        /*
+            The listIterator method create a new list iterator that is
+                positioned at the front of the list.
+            The "|" in the commend indicates the iterator position.
+        */
+        ListIterator<String> iterator = staff.listIterator();   // |DHRT
+
+        /*
+            The next method advances the iterator over the next element
+                in the list.
+        */
+        iterator.next();    // D|HRT
     }
 }
