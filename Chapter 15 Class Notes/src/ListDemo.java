9fe4527edf6b5dfac4f8ce0a6f40ca63e09550f9
Geoff Schmit <gcschmit@gmail.com>
2021-09-10 15:03:44 -0500

    finish notes on list iterators

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -32,5 +32,95 @@ public class ListDemo
                 in the list.
         */
         iterator.next();    // D|HRT
+
+        /* 
+            The next method also returns the element that the iterator
+                is passing.
+         */
+        String name = iterator.next();      // DH|RT
+        System.out.println(name);
+        System.out.println("Expected: Harry");
+
+        /*
+            The add method inserts an element at the iterator position.
+            The iterator is then positioned after the element that was added.
+                (The text is a bit confusing on this point...)
+        */
+        iterator.add("Juliet");     // DHJ|RT
+        iterator.add("Nina");          // DHJN|RT
+
+        /*
+            The remove method removes the element returned by the last
+                call to next or previous.
+            The remove method can only be called once after calling next
+                or previous.
+            The remove method cannot be called after calling add.
+        */
+        iterator.next();    // DHJNR|T
+        iterator.remove();  // DHJN|T
+
+        System.out.println(staff);
+        System.out.println("Expected: [Diana, Harry, Juliet, Nina, Tom]");
+
+        /* 
+            The set method updates the element returned by the last call
+                to next or previous.
+         */
+        iterator.previous();    // DHJ|NT
+        iterator.set("Albert"); // DHJ|AT
+
+        /*
+            The hasNext method is often used in the context of a while loop.
+        */
+        iterator = staff.listIterator();    // |DHJAT
+        while(iterator.hasNext())
+        {
+            String n =  iterator.next();
+            if(n.equals("Juliet"))
+            {
+                                        // DHJ|AT
+                iterator.remove();      // DH|AT
+            }
+        }
+                                        // DHAT|
+        
+        /*
+            Enhanced for loops work with linked lists!
+        */
+        for(String n : staff)
+        {
+            System.out.println(n + " ");
+        }
+        System.out.println("Expected: Diana Harry Albert Tom");
+
+        /*
+            ConcurrentModificationException
+
+            Cannot modify a linked list while also using an iterator
+                unless you use that iterator to do so
+        */
+        iterator = staff.listIterator();
+        while(iterator.hasNext())
+        {
+            String n = iterator.next();
+            if(n.equals("Harry"))
+            {
+                //staff.remove("Diana");
+            }
+        }
+
+        /*
+            ConcurrentModificationException
+
+            The enhanced for loop automatically  creates an iteartor!
+            That's how it works!
+        */
+        for(String n : staff)
+        {
+            if(n.equals("Harry"))
+            {
+                staff.add("Charlie");
+            }
+        }
     }
 }
