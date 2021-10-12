b53bf132ec75b55ed21f401630b300170b910b04
Geoff Schmit <gcschmit@gmail.com>
2021-10-12 15:03:18 -0500

    add example of stack and circular array queue

--- Chapter 16 Class Notes/src/CircularArrayQueue.java
+++ Chapter 16 Class Notes/src/CircularArrayQueue.java
@@ -6,44 +6,61 @@ import java.util.NoSuchElementException;
 public class CircularArrayQueue
 {
     private Object[] elements;
-    //private data
-
-
+    private int head;
+    private int tail;
+    private int currentSize;
 
     /**
         Constructs an empty queue.
     */
-
-
-
-
-
+    public CircularArrayQueue()
+    {
+        final int INITIAL_SIZE = 5;
+        this.elements = new Object[INITIAL_SIZE];
+        this.head = 0;
+        this.tail = 0;
+        this.currentSize = 0;
+    }
 
     /**
         Checks whether this queue is empty.
         @return true if this queue is empty
     */
-
-
-
+    public boolean empty()
+    {
+        return (this.currentSize == 0);
+    }
 
     /**
         Adds an element to the tail of this queue.
         @param newElement the element to add
     */
+    public void add(Object newElement)
+    {
+        this.growIfNecessary();
 
-
-
-
+        this.currentSize++;
+        this.elements[this.tail] = newElement;
+        this.tail++;
+        this.tail %= this.elements.length;
+    }
 
     /**
         Removes an element from the head of this queue.
         @return the removed element
     */
+    public Object remove()
+    {
+        if(this.empty())
+        {
+            throw new NoSuchElementException();
+        }
 
-
-
-
+        this.currentSize--;
+        Object element = this.elements[this.head];
+        this.head = (this.head + 1) % this.elements.length;
+        return element;
+    }
 
     /**
         Grows the element array if the current size equals the capacity.
